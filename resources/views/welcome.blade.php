3ce382610e827c7d8d4a2ad7503ec9a996dded73
emmyabba <emmyabba@gmail.com>
2020-09-03 11:32:10 +0100

    index page and hero section updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
         <!-- Hero Start -->
-        <section class="bg-half-170 d-table w-100" id="home">
+        <section class="bg-half-170 border-bottom agency-wrapper d-table w-100" id="home">
             <div class="container">
                 <div class="row align-items-center">
                     <div class="col-lg-7 col-md-7">
@@ -19,9 +19,7 @@
 
                     <div class="col-lg-5 col-md-5 mt-4 pt-2 mt-sm-0 pt-sm-0">
                         <div class="classic-app-image position-relative">
-                            <div class="bg-app-shape position-relative">
-                                <img src="{!! asset('theme/images/logeak-hero-image-1.svg')!!}" alt="">
-                            </div>
+                                <img src="{!! asset('theme/images/logeak-hero-image-1.svg')!!}" class="img-fluid" alt="">
                         </div>
 
                     </div><!--end col-->
