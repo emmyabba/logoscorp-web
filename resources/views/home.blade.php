0aac09d19ded86b6acb2aeb47f13dfef9eae418f
“emmyabba” <emmyabba@gmail.com>
2021-11-01 02:10:21 +0100

    updated front page servces

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -53,40 +53,39 @@
                 <div class="solutions-title">
                     <span></span>
                     <h3>Solutions tailored to fit your world</h3>
-                    <p>We are a technology company that provide cutting edge solutions, helping organizations and individuals achieve more with Logeak as a trusted and valued partner</p>
+                    <p>We are a technology company that provide cutting edge solutions, helping organizations and individuals achieve more with Logeak as a trusted and valued partner.<br></p>
                 </div>
 
                 <div class="row">
-                    <div class="col-lg-4 col-md-6">
-                        <div class="solutions-area-content">
-                            <div class="icon">
-                                <i class="flaticon-data"></i>
-                            </div>
-                            <h3>User-Friendly Behaviour For Business mind people</h3>
-                            <p>Lorem ipsum dolor sit amet consecte adipiscing elit sed do eiusincidunt.</p>
-                            <a href="single-services.html" class="">Read More</a>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-4 col-md-6">
-                        <div class="solutions-area-content">
-                            <div class="icon">
-                                <i class="flaticon-goal"></i>
-                            </div>
-                            <h3>Business Development All Kind of Business</h3>
-                            <p>Lorem ipsum dolor sit amet consecte adipiscing elit sed do eiusincidunt.</p>
-                            <a href="single-services.html" class="">Read More</a>
-                        </div>
-                    </div>
-
                     <div class="col-lg-4 col-md-6 offset-md-3 offset-lg-0">
                         <div class="solutions-area-content">
                             <div class="icon">
-                                <i class="flaticon-modules"></i>
+                                <i class="flaticon-monitor"></i>
                             </div>
-                            <h3>Management is no risk here if We support you</h3>
-                            <p>Lorem ipsum dolor sit amet consecte adipiscing elit sed do eiusincidunt.</p>
+                            <h3>eLearning </h3>
+                            <p>Learning and development services such as instructional design, course development, and learning technologies like portals and content platforms</p>
                             <a href="single-services.html" class="">Read More</a>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="solutions-area-content">
+                            <div class="icon">
+                                <i class="flaticon-it"></i>
+                            </div>
+                            <h3>Enterprise Software Development </h3>
+                            <p>Automate your business processes with scalable software that improves key facets of your enterprise with industry-specific design, build, and deployment of enterprise software solution.</p>
+                            <a href="#" class="">Read More</a>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6">
+                        <div class="solutions-area-content">
+                            <div class="icon">
+                                <i class="fa fa-mobile"></i>
+                            </div>
+                            <h3>Mobile App Development</h3>
+                            <p>Go mobile-first with intuitively designed degital products that enable seamless experience across devices and operating systems to create impactful apps that fit your brand and industry.</p>
+                            <a href="#" class="">Read More</a>
                         </div>
                     </div>
                 </div>
